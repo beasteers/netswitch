5ccb55c3a817fc2a7abb929ac1f2a9a3a60e2e87
sonycnode-dca632ceb541 <sonyc.project@gmail.com>
2021-04-16 17:52:22 -0400

    adding cell ccid and improving chat prompt

--- netswitch/cell.py
+++ netswitch/cell.py
@@ -17,7 +17,10 @@ class Cell(serial.Serial):
                  timeout=DEFAULT_TIMEOUT, **kw):
         super(Cell, self).__init__(device, baudrate, timeout=timeout, **kw)
 
-    def msgsend(self, cmd, length=64, end='\r'):
+    def msgsend(self, cmd, length=64, end='\r', add_at=True):
+        cmd = cmd.upper()
+        if not cmd.startswith('AT'):
+            cmd = 'AT'+cmd
         self.write((cmd + end).encode())
         return self.read(length).decode("utf-8")
 
@@ -45,36 +48,77 @@ class Cell(serial.Serial):
             value = int(msg.split(': ')[1].split(',')[0].strip())
             return 2 * value - 112
 
+    @property
+    def ccid(self):
+        '''Return SIM CCID.'''
+        msg = self.ping() and self.send("AT+CCID")
+        if msg:
+            ccid = msg.split(': ')[1].strip()
+            return ccid
+
     def chat(self):
         try:
-            print('Opened Chat:')
+            import readline
+            import atexit, os
+            readline.parse_and_bind('tab: complete')
+            readline.parse_and_bind('set editing-mode vi')
+            histfile = os.path.join(os.path.expanduser("~"), ".netswitch_at_history")
+            try:
+                readline.read_history_file(histfile)
+                # default history len is -1 (infinite), which may grow unruly
+                readline.set_history_length(1000)
+            except FileNotFoundError:
+                pass
+            atexit.register(readline.write_history_file, histfile)
+        except ImportError:
+            pass
+        try:
+            print('''
+AT Chat! Reference: https://en.wikipedia.org/wiki/Hayes_command_set
+         (AT prefix optional, e.g. +csq -> AT+CSQ)
+Examples:
+*** +csq     # Get signal strength. (rssi(more=better), ber(less=better))
+*** +ccid    # Get sim CCID
+*** # ummmm I mean I don't really know what to do so..
+            '''.lstrip())
+
             while True:
                 cmd = input('*** ')
                 if cmd.lower() == 'exit':
                     break
-                print(self.send(cmd))
+                print(self.msgsend(cmd))
         except KeyboardInterrupt:
-            pass
+            print()
+        print('bye!')
+    
+    @classmethod
+    def find_device(cls, device_pattern=DEFAULT_DEVICE_PATTERN, retry=3, *a, **kw):
+        for dev in sorted(glob.glob(device_pattern)):
+            try:
+                cell = cls(dev, *a, **kw)
+                if any(cell.send('ATZ') for _ in range(retry or 1)):
+                    return cell
+                cell.close()
+            except BrokenPipeError:
+                pass
+        raise OSError('No device found matching {}.'.format(device_pattern))
 
+    @staticmethod
+    def list_devices(device_pattern=DEFAULT_DEVICE_PATTERN):
+        return glob.glob(device_pattern)
 
-
-def devices(device_pattern=DEFAULT_DEVICE_PATTERN):
-    return glob.glob(device_pattern)
-
-@contextmanager
-def cell_device(device_pattern=DEFAULT_DEVICE_PATTERN, retry=3, *a, **kw):
-    for dev in glob.glob(device_pattern):
-        with Cell(dev, *a, **kw) as cell:
-            if any(cell.send('ATZ') for _ in range(retry or 1)):
-                yield cell
-                return
-    raise OSError('No device found matching {}.'.format(device_pattern))
+devices = Cell.list_devices
+cell_device = Cell.find_device
 
 def signal_strength(com_device=DEFAULT_DEVICE):
-    with cell_device(com_device) as cell:
+    with Cell.find_device(com_device) as cell:
         return cell.quality
+
+def sim_ccid(com_device=DEFAULT_DEVICE):
+    with Cell.find_device(com_device) as cell:
+        return cell.ccid
 
 
 if __name__ == '__main__':
     import fire
-    fire.Fire(cell_device)
+    fire.Fire(Cell)
